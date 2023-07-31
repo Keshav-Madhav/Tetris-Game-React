4fdc9b08fd1f4b70483c41098e034abf07edd70f
Keshav <keshav2552003@gmail.com>
2023-07-31 18:44:48 +0530

    Added block collisions between block-wall and block-block

--- Tetris/src/hooks/usePlayer.js
+++ Tetris/src/hooks/usePlayer.js
@@ -1,27 +1,28 @@
-import { useCallback, useState } from "react";
-import { TETROMINOS,randomTetromino } from "../tetrominos";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { useState, useCallback } from 'react';
 
-export const usePlayer = ()=>{
+import { TETROMINOS, randomTetromino } from '../tetrominos';
+import { STAGE_WIDTH } from '../gameHelpers';
+
+export const usePlayer = () => {
     const [player, setPlayer] = useState({
-        pos : {x:0, y:0},
+        pos: { x: 0, y: 0 },
         tetromino: TETROMINOS[0].shape,
         collided: false,
     });
 
-    const updatePlayerPos= ({x, y, collided}) =>{
-        setPlayer(prev=>({
-            ...prev,
-            pos:{x: (prev.pos.x += x), y: (prev.pos.y += y)},
-            collided,
+    const updatePlayerPos = ({ x, y, collided }) => {
+        setPlayer(prev => ({
+        ...prev,
+        pos: { x: (prev.pos.x += x), y: (prev.pos.y += y)},
+        collided,
         }))
     }
 
-    const resetPlayer = useCallback(()=>{
+    const resetPlayer = useCallback(() => {
         setPlayer({
-            pos: {x: STAGE_WIDTH/2-2, y: 0},
-            tetromino: randomTetromino().shape,
-            collided: false,
+        pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+        tetromino: randomTetromino().shape,
+        collided: false,
         })
     }, [])
 
